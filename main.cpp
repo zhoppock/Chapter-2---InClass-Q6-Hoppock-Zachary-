1f1231df8bdbdf341a8361dc5b3773aac2623776
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 03:01:49 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Hoppock, Zachary
+//address.cpp
+//9/12/2020
+//Outputs details of a street address
+//Version #1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "What is your street address?" << endl;
+  cout << "123 First Street" << endl;
+  cout << "What is your city, state, and Zip?"<< endl;
+  cout << "Alta Loma, CA 99999";
+
+  return 0;
+}
